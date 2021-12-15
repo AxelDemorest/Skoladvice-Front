480d233a92fa5255736a06c09e784eeffc8b9039
AxelDemorest <axel.demorest@gmail.com>
2021-12-15 09:30:37 +0100

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <p className="bg-danger">Loading...</p>
         <a
           className="App-link"
           href="https://reactjs.org"
